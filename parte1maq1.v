a25349ffd7060f966855447c0dcd705195382cb6
Felipi12 <felipemartinsldemorais@gmail.com>
2021-03-26 18:16:47 -0300

    First commit
    Commiting first changes

---
+++ parte1maq1.v
@@ -0,0 +1,93 @@
+module parte1maq1(
+  input [0:0] KEY,
+  input [1:0] SW,
+);
+
+  // Processor States
+  localparam Invalid = 2'b00;
+  localparam Exclusive = 2'b01;
+  localparam Shared = 2'b10;
+
+  // Processor Messages
+  localparam Place_Read_Miss_On_Bus = 3'b001;
+  localparam Place_Invalidate_On_Bus = 3'b010;
+  localparam Place_Write_Miss_On_Bus = 3'b011;
+
+  localparam Write_Back_Block = 3'b100;
+  localparam Write_Back_Cache_Block = 3'b101;
+
+  localparam Empty = 3'b000;
+
+  // Operations
+  localparam RM = 2'b00;
+  localparam RH = 2'b01;
+  localparam WM = 2'b10;
+  localparam WH = 2'b11;
+
+  reg [1:0] state;
+  reg [2:0] msg;
+
+  initial
+    state <= 1;
+
+  always @(posedge KEY[0])
+    case(state)
+      Invalid:
+        case(SW)
+          RM, RH:
+            begin
+              state <= Shared;
+              msg <= Place_Read_Miss_On_Bus;
+            end
+          WM, WH:
+            begin
+              state <= Exclusive;
+              msg <= Place_Write_Miss_On_Bus;
+            end
+        endcase
+
+      Exclusive:
+        case(SW)
+          WH, RH:
+            begin
+              state <= Exclusive;
+              msg <= Empty;
+            end
+          WM:
+            begin
+              state <= Exclusive;
+              msg <= Write_Back_Cache_Block;
+            end
+          RM:
+            begin
+              state <= Shared;
+              msg <= Write_Back_Block;
+            end
+        endcase
+
+      Shared:
+        case(SW)
+          RH:
+            begin
+              state <= Shared;
+              msg <= Empty;
+            end
+          RM:
+            begin
+              state <= Shared;
+              msg <= Place_Read_Miss_On_Bus;
+            end
+          WH:
+            begin
+              state <= Exclusive;
+              msg <= Place_Invalidate_On_Bus;
+            end
+          WM:
+            begin
+              state <= Exclusive;
+              msg <= Place_Write_Miss_On_Bus;
+            end
+        endcase
+    endcase
+
+endmodule
---
+++ parte1maq2.v
@@ -0,0 +1,68 @@
+module parte1maq2 (
+  input [0:0] KEY,
+  input [1:0] SW,
+);
+
+  // Processor States
+  localparam Invalid = 2'b00;
+  localparam Exclusive = 2'b01;
+  localparam Shared = 2'b10;
+
+  // Processor Messages
+  localparam Empty = 1'b0;
+  localparam Write_Back_Block = 1'b1;
+
+  // Operations
+  localparam RM = 2'b00;
+  localparam Invalidate = 2'b01;
+  localparam WM = 2'b10;
+
+  reg [1:0] state;
+  reg msg;
+
+  initial
+    state <= 2;
+
+  always @(posedge KEY[0])
+    case(state)
+      Invalid:
+        begin
+          state <= Invalid;
+          msg <= Empty;
+        end
+
+      Exclusive:
+        case(SW)
+          WM:
+            begin
+              state <= Invalid;
+              msg <= Write_Back_Block;
+            end
+          RM:
+            begin
+              state <= Shared;
+              msg <= Write_Back_Block;
+            end
+          default:
+            begin
+              state <= Exclusive;
+              msg <= Empty;
+            end
+        endcase
+
+      Shared:
+        case(SW)
+          WM, Invalidate:
+            begin
+              state <= Invalid;
+              msg <= Empty;
+            end
+          default:
+            begin
+              state <= Shared;
+              msg <= Empty;
+            end
+        endcase
+    endcase
+
+endmodule
